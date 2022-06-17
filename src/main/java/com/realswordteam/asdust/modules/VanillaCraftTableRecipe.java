ffd1d93baddcaa48d8440307118196e5f997b4b2
ZHEN_ZI233 <1994577746@qq.com>
2022-06-17 23:07:14 +0800

    Add removeRecipe method

--- src/main/java/com/realswordteam/asdust/modules/VanillaCraftTableRecipe.java
+++ src/main/java/com/realswordteam/asdust/modules/VanillaCraftTableRecipe.java
@@ -1,10 +1,18 @@
 package com.realswordteam.asdust.modules;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 public class VanillaCraftTableRecipe {
+    /**
+     * Use it in {@link com.realswordteam.asdust.recipes.RecipesLoader} and fill {@link #removeCraftTableRecipe(String)} name
+     * @param name recipe name
+     */
     public static void removeCraftTableRecipe(String name)
     {
         IForgeRegistryModifiable modifiable = (IForgeRegistryModifiable) ForgeRegistries.RECIPES;

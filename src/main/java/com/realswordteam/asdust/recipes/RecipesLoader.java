ebadc376c62131507ca54be545778459c8a9a814
ZHEN_ZI233 <1994577746@qq.com>
2022-06-17 23:13:23 +0800

    Add removeRecipe method

--- src/main/java/com/realswordteam/asdust/recipes/RecipesLoader.java
+++ src/main/java/com/realswordteam/asdust/recipes/RecipesLoader.java
@@ -50,6 +50,7 @@ public class RecipesLoader {
      */
     private final String[] vanillaRecipe =
             {
-        "minecraft:stonebrick",
+                    "minecraft:stonebrick",
+                    "minecraft:furnace"
             };
 }

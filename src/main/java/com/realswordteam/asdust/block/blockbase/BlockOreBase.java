1b4dec141ef52601b4f78b04acecae5dcefcf6fc
ZHEN_ZI233 <1994577746@qq.com>
2022-08-22 13:55:47 +0800

    add simple drops for ore

--- src/main/java/com/realswordteam/asdust/block/blockbase/BlockOreBase.java
+++ src/main/java/com/realswordteam/asdust/block/blockbase/BlockOreBase.java
@@ -1,16 +1,35 @@
 package com.realswordteam.asdust.block.blockbase;
 
+import com.realswordteam.asdust.block.ore.OreBlockLoader;
 import com.realswordteam.asdust.block.ore.OreHarvestLevelDictionary;
+import com.realswordteam.asdust.items.ItemLoader;
 import com.realswordteam.asdust.misc.creativetabs.CreativeTabLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
+import java.util.Random;
 
 public class BlockOreBase extends Block {
 
-    public BlockOreBase(){
+    public BlockOreBase() {
         super(Material.ROCK);
         this.setCreativeTab(CreativeTabLoader.TAB_ORE);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        if (this == OreBlockLoader.CASSITERITE) {
+            return ItemLoader.BRIQUETTE;
+        }
+        else
+        {
+            return ItemLoader.ANIMAL_FAT;
+        }
     }
 
 }
